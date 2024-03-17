bf79dce012682495c7704cf7d1eba98bc2eff6b3
hoangkhanhvan <hoangkhanhvan.uel@gmail.com>
2024-03-17 15:04:42 +0700

    add new ticket component

---
+++ src/app/find-bus-trip/find-bus-trip.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TicketItemComponent } from './ticket-item/ticket-item.component';
+
+
+
+@NgModule({
+  declarations: [
+    TicketItemComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class FindBusTripModule { }
---
+++ src/app/find-bus-trip/ticket-item/ticket-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ticket-item',
+  templateUrl: './ticket-item.component.html',
+  styleUrl: './ticket-item.component.scss'
+})
+export class TicketItemComponent {
+
+}
