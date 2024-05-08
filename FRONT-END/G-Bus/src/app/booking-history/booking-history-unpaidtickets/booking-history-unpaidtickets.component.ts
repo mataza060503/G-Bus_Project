8a28f0824c3758232e9c1a422c4333c005d09f9a
vohoanglam <vohoanglam060503@gmail.com>
2024-05-08 20:06:23 +0700

    Load Order by status

--- FRONT-END/G-Bus/src/app/booking-history/booking-history-unpaidtickets/booking-history-unpaidtickets.component.ts
+++ FRONT-END/G-Bus/src/app/booking-history/booking-history-unpaidtickets/booking-history-unpaidtickets.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DataService } from '../../../services/Data.service';
+import { OrderTicket } from '../../../models/ticket';
 
 @Component({
   selector: 'app-booking-history-unpaidtickets',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './booking-history-unpaidtickets.component.scss'
 })
 export class BookingHistoryUnpaidticketsComponent {
+  orders!: OrderTicket[]
+  errMessage: string = ""
+  accountId: string = "account1"
 
+  constructor(private dataService: DataService) {
+    this.loadOrderData(this.accountId, "Unpaid")
+  }
+  loadOrderData(accountId: string, status: string) {
+    this.dataService.getAllOrderByStatus(accountId, status).subscribe({
+      next: (data) => {
+        this.orders = data
+        console.log(data)
+      }, error: (err) => {
+        this.errMessage = err
+      }
+    })
+  }
 }
